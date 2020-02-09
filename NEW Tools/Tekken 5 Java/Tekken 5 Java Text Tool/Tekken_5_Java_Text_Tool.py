b5f9a0a4365da674a1a9af248cbabce7d5324127
bartlomiej.duda <ikskoks@gmail.com>
2020-02-09 08:52:53 +0100

    Tekken 5 Java Text Tool 09.02.2020 08:52

--- NEW Tools/Tekken 5 Java/Tekken 5 Java Text Tool/Tekken_5_Java_Text_Tool.py
+++ NEW Tools/Tekken 5 Java/Tekken 5 Java Text Tool/Tekken_5_Java_Text_Tool.py
@@ -5,10 +5,12 @@
 
 # Ver    Date        Name
 # v0.1   05.02.2020  Bartlomiej Duda
+# v0.2   08.02.2020  Bartlomiej Duda
+# v0.3   09.02.2020  Bartlomiej Duda
 
 
 
-VERSION_NUM = "v0.1"
+VERSION_NUM = "v0.3"
 
 
 import os
@@ -20,8 +22,16 @@ import webbrowser
 import traceback
 import stat
 
+
+
+def bd_logger(in_str):
+    import datetime
+    now = datetime.datetime.now()
+    print(now.strftime("%d-%m-%Y %H:%M:%S") + " " + in_str)    
+
+
 def read_loc(p_input_loc_filepath, p_out_filepath):
-    print ("starting Crash Java sprite read...")
+    bd_logger("Starting Tekken 5 loc read...")
     loc_file = open(p_input_loc_filepath, 'rb') 
     out_file = open(p_out_filepath, 'wt+') 
     str_arr = []
@@ -36,26 +46,65 @@ def read_loc(p_input_loc_filepath, p_out_filepath):
         #print("Str: " + str(str_read))
         str_arr.append(str_read)
         
-        
-    #out_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    #out_file.write("<root>\n")
     for i in range(num_of_text_strings):
         out_file.write("BD_TRANSLATE_TEXT_TAG=")
         out_file.write( str_arr[i].replace("\n", "\\n")
+                                  .replace("™", "<TM_SIGN>")
+                                  .replace("©", "<CP_SIGN>")
                        
                        )
         out_file.write("\n")
-        #out_file.write("</BD_TRANSLATE_TEXT_TAG" + str(i) + ">\n")
-        
-    #out_file.write("</root>")
+
     print("Offset: " + str(loc_file.tell()))
     
-    print ("Ending Crash Java sprite read...")
+    bd_logger("Ending Tekken 5 loc read...")
+    
+
+
+def write_loc(p_input_ini_filepath, p_loc_out_filepath):
+    bd_logger("Starting Tekken 5 loc write...")
+    loc_file = open(p_loc_out_filepath, 'rb') 
+    ini_file = open(p_input_ini_filepath, 'rt', encoding="utf-8")  #  windows-1250 or utf-8
+    
+    size_of_the_header = struct.unpack('>i', loc_file.read(4))[0]
+    num_of_text_strings = struct.unpack('>i', loc_file.read(4))[0]
+    header_data = loc_file.read(size_of_the_header)
+    loc_file.close()
+    
+    cnt = 0
+    line_arr = []
+    for line in ini_file:
+        cnt += 1
+        line = line.split("BD_TRANSLATE_TEXT_TAG=")[1]
+        line_arr.append(line)
+        #print(str(cnt) + ") " + line)
+    ini_file.close()
+    
+    loc_file = open(p_loc_out_filepath + "temp111", 'wb+') 
+    loc_file.write(struct.Struct(">i").pack(size_of_the_header))
+    loc_file.write(struct.Struct(">i").pack(num_of_text_strings))
+    loc_file.write(header_data)
+    
+    for line_a in line_arr:
+        
+        line_res = ( line_a.rstrip("\n").encode("utf-8").replace(b"\\n", b"\n")   #  windows-1250 or utf-8
+                                        .replace(b"<TM_SIGN>", b"\xE2\x84\xA2")
+                                        .replace(b"<CP_SIGN>", b"\xC2\xA9")
+                     )
+        
+        line_res_len = len(line_res)
+        loc_file.write(struct.Struct(">H").pack(line_res_len))
+        loc_file.write(line_res )   
+    
+    bd_logger("Ending Tekken 5 loc write...")
     
     
     
-    
-    
-input_loc_filepath = "C:\\Users\\Arek\\Desktop\\TRAD_english.loc"
-out_filepath = "C:\\Users\\Arek\\Desktop\\OUT.ini"
-read_loc(input_loc_filepath, out_filepath)
+#input_loc_filepath = "C:\\Users\\Arek\\Desktop\\TRAD_english.loc"
+#out_filepath = "C:\\Users\\Arek\\Desktop\\OUT.ini"
+#read_loc(input_loc_filepath, out_filepath)
+
+
+input_ini_filepath = "C:\\Users\\Arek\\Desktop\\OUT.ini"
+loc_out_filepath = "C:\\Users\\Arek\\Desktop\\TRAD_english.loc"
+write_loc(input_ini_filepath, loc_out_filepath)
