db75deb46d89b886f5e255d99693509b6daa6611
bartlomiej.duda <ikskoks@gmail.com>
2020-02-17 00:18:37 +0100

    Silent Hill SM Text Tool 17.02.2020 00:18

--- NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_Text_Tool.py
+++ NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_Text_Tool.py
@@ -6,8 +6,9 @@
 # Ver    Date        Author
 # v0.1   16.02.2020  Bartlomiej Duda
 # v0.2   16.02.2020  Bartlomiej Duda
+# v0.3   17.02.2020  Bartlomiej Duda
 
-VERSION_NUM = "v0.2"
+VERSION_NUM = "v0.3"
 
 import os
 import sys
@@ -62,21 +63,39 @@ def read_SUB(in_sub_filepath):
                     break
                 else:
                     str1 += decode_char(ch)
-            print( str(i+1) + ") " + str1 )  
+            #print( str(i+1) + ") " + str1 )  
+            text_arr.append(str1)
         except:
             offset_end = sub_file.tell()
             print("End of file! Offset: " + str(offset_end) )
                 
         
     print("off_end: " + str( sub_file.tell() ) )
-    
-    
     sub_file.close()
+    
+    
+    ini_file = open(in_sub_filepath + ".ini", 'wb+') 
+    for i in range(num_of_strings):
+        #print(text_arr[i])
+        ini_file.write(b"BD_TRANSLATE_TEXT=")
+        ini_file.write(text_arr[i].encode("utf-8", errors="xmlcharrefreplace") )
+        ini_file.write(b"\n")
+    
+    ini_file.close()
+    
     bd_logger("Ending read_SUB function...")
 
 
 
 #  read SUB 
 #p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\Silent_Hill_SM\\out_s2\\DATA_47.sub"
-p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\Silent_Hill_SM\\out_s2\\DATA_1323.sub"
+p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\SUBs\\DATA_1323.sub"
 read_SUB(p_in_sub_filepath)
+
+
+#file_arr_all = ["DATA_47", "DATA_48", "DATA_49", "DATA_50", "DATA_51", "DATA_52", "DATA_1317",
+            #"DATA_1318", "DATA_1319", "DATA_1320", "DATA_1321", "DATA_1322", "DATA_1323",
+            #"DATA_1324", "DATA_1325" ]
+
+## read SUB loop
+#for file_i in file_arr_all:
