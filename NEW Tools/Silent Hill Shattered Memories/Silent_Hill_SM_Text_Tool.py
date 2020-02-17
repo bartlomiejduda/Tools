19156673e5579dd077b3949dbe6cd5f64fc973e0
bartlomiej.duda <ikskoks@gmail.com>
2020-02-17 10:37:15 +0100

    Silent Hill SM Text Tool 17.02.2020 10:37

--- NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_Text_Tool.py
+++ NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_Text_Tool.py
@@ -7,8 +7,9 @@
 # v0.1   16.02.2020  Bartlomiej Duda
 # v0.2   16.02.2020  Bartlomiej Duda
 # v0.3   17.02.2020  Bartlomiej Duda
+# v0.4   17.02.2020  Bartlomiej Duda
 
-VERSION_NUM = "v0.3"
+VERSION_NUM = "v0.4"
 
 import os
 import sys
@@ -64,6 +65,7 @@ def read_SUB(in_sub_filepath):
                 else:
                     str1 += decode_char(ch)
             #print( str(i+1) + ") " + str1 )  
+            str1 = ( str1.replace("\n", "\\n").replace("\r", "\\r")  )
             text_arr.append(str1)
         except:
             offset_end = sub_file.tell()
@@ -79,6 +81,9 @@ def read_SUB(in_sub_filepath):
         #print(text_arr[i])
         ini_file.write(b"BD_TRANSLATE_TEXT=")
         ini_file.write(text_arr[i].encode("utf-8", errors="xmlcharrefreplace") )
+        #ini_file.write(text_arr[i].encode("utf-8", errors="namereplace") )
+        #ini_file.write(text_arr[i].encode("utf-8", errors="backslashreplace") )
+        
         ini_file.write(b"\n")
     
     ini_file.close()
@@ -86,16 +91,46 @@ def read_SUB(in_sub_filepath):
     bd_logger("Ending read_SUB function...")
 
 
+def read_SUB_ALL():
+    file_arr_all = ["DATA_47", "DATA_48", "DATA_49", "DATA_50", "DATA_51", "DATA_52", "DATA_1317",
+                "DATA_1318", "DATA_1319", "DATA_1320", "DATA_1321", "DATA_1322", "DATA_1323",
+                "DATA_1324", "DATA_1325" ]
+    
+    file_arr_eng = ["DATA_47", "DATA_1317", "DATA_1318"]
+    
+    # read SUB loop
+    for file_i in file_arr_eng:
+        p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\SUB_ENG\\" + file_i + ".sub"
+        read_SUB(p_in_sub_filepath)
+    print("FINISHED ALL!")
+    
+
+def write_SUB(in_ini_filepath):
+    bd_logger("Starting write_SUB function...")
+    ini_file = open(in_ini_filepath, 'rt')
+    
+    ini_line_arr = []
+    for line in ini_file:
+        line_i = line.split("BD_TRANSLATE_TEXT=")[1]
+        ini_line_arr.append(line_i)
+        
+    for line in ini_line_arr:
+        print(line)
+    
+    bd_logger("Ending write_SUB function...")
+
+
+    
 
 #  read SUB 
 #p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\Silent_Hill_SM\\out_s2\\DATA_47.sub"
-p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\SUBs\\DATA_1323.sub"
-read_SUB(p_in_sub_filepath)
+#p_in_sub_filepath = "C:\\Users\\Arek\\Desktop\\SUBs\\DATA_1323.sub"
+#read_SUB(p_in_sub_filepath)
+
+#read_SUB_ALL()
 
 
-#file_arr_all = ["DATA_47", "DATA_48", "DATA_49", "DATA_50", "DATA_51", "DATA_52", "DATA_1317",
-            #"DATA_1318", "DATA_1319", "DATA_1320", "DATA_1321", "DATA_1322", "DATA_1323",
-            #"DATA_1324", "DATA_1325" ]
 
-## read SUB loop
-#for file_i in file_arr_all:
+#write SUB
+p_in_ini_filepath = "C:\\Users\\Arek\\Desktop\\SUB_ENG\\DATA_1317.sub.ini"
+write_SUB(p_in_ini_filepath)
