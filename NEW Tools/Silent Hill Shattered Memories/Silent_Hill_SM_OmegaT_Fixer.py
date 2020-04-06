85dc179e71cbfed81507b60094df0ecbf8e409c7
bartlomiej.duda <ikskoks@gmail.com>
2020-04-06 19:54:37 +0200

    Silent Hill OmegaT Fixer v0.2

--- NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_OmegaT_Fixer.py
+++ NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_OmegaT_Fixer.py
@@ -5,9 +5,10 @@
 
 # Ver    Date        Author
 # v0.1   05.04.2020  Bartlomiej Duda
+# v0.2   06.04.2020  Bartlomiej Duda
 
 
-VERSION_NUM = "v0.1"
+VERSION_NUM = "v0.2"
 
 import os
 import sys
@@ -30,9 +31,8 @@ def fix_INI_file(in_INI_filepath, out_INI_filepath):
     
     in_INI_file = open(in_INI_filepath, 'rt')
     
+    
     out_arr = []
-    temp_arr = []
-    line_flag = 0
     for i in range(count_lines):
         line = in_INI_file.readline()
         print (i)
@@ -40,54 +40,24 @@ def fix_INI_file(in_INI_filepath, out_INI_filepath):
         if line == '':
             break
         
-        if i == 20:
+        if ((line.startswith("[") and "]" in line)   or   ("REF:" in line)    or   (line == "\n") 
+            or   (line.startswith("<c=1>"))  
+            or   (line.startswith("<c=2>")) 
+            or   (line.startswith("<c=3>"))  
+            or   (line.startswith("<b=9>"))  
+            
+            ):   
             pass
-        
-        if (line.startswith("[") and "]" in line)   or   ("REF:" in line)    or   (line == "\n"):
-            out_arr.append(line)
-            #print ("temp1: " + line)
-
         else:
-            temp_str = ""
-            line2 = ""
-            temp_str = line.strip("\n")
+            line = "BD_TRANSLATE_TEXT=" + line
             
-            wh_counter = 0
-            while 1:                #     not ( (line.startswith("[") and "]" in line)   or ("REF:" in line)    or (line == "\n") ) :
-                wh_counter += 1
-                line = in_INI_file.readline()
-                if (line.startswith("[") and "]" in line)   or   ("REF:" in line)    or   (line == "\n"):
-                    #if line != "\n":
-                    line2 += line
-                    
-                    #out_arr.append(line )
-                    break
-                
-                
-                count_lines -= 1
-                #if wh_counter == 1:
-                    #temp_str += line.strip("\n") + " <IKS_ENT> "
-                    
-                #else:
-                    #temp_str += line
-                    
-                temp_str += line.strip("\n") + " <IKS_ENT> "    
-                #print(line)
-            
-
-            temp_str += "\n"
-                
-            out_arr.append(temp_str)
-            out_arr.append(line2 )
-            #print ("temp2: " + temp_str)
-     
+        out_arr.append(line)
      
      
     out_INI_file = open(out_INI_filepath, 'wt+')  
-    strr_blob = ""
-    for strr in out_arr:
-        strr_blob += strr
-    out_INI_file.write(strr_blob)
+
+    for line in out_arr:
+        out_INI_file.write(line)
                 
             
     out_INI_file.close()
@@ -96,6 +66,6 @@ def fix_INI_file(in_INI_filepath, out_INI_filepath):
 
 
 #OmegaT fix
-p_in_INI_filepath = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\target\\PSP_USA\\2C238264.ini"
-p_out_INI_filepath = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\target\\PSP_USA\\2C238264_fixed.ini"
+p_in_INI_filepath = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\source\\PSP_USA\\2C238264.ini"
+p_out_INI_filepath = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\source\\PSP_USA\\2C238264_fixed.ini"
 fix_INI_file(p_in_INI_filepath, p_out_INI_filepath)
