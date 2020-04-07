c9eec8f4c01c1388b4baff9edea19104904eb83c
bartlomiej.duda <ikskoks@gmail.com>
2020-04-07 10:43:30 +0200

    Silent Hill SM OmegaT Fixer v0.3

--- NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_OmegaT_Fixer.py
+++ NEW Tools/Silent Hill Shattered Memories/Silent_Hill_SM_OmegaT_Fixer.py
@@ -6,9 +6,10 @@
 # Ver    Date        Author
 # v0.1   05.04.2020  Bartlomiej Duda
 # v0.2   06.04.2020  Bartlomiej Duda
+# v0.3   07.04.2020  Bartlomiej Duda
 
 
-VERSION_NUM = "v0.2"
+VERSION_NUM = "v0.3"
 
 import os
 import sys
@@ -35,19 +36,18 @@ def fix_INI_file(in_INI_filepath, out_INI_filepath):
     out_arr = []
     for i in range(count_lines):
         line = in_INI_file.readline()
-        print (i)
+        print ("Line: " + str(i+1))
         
         if line == '':
             break
         
         if ((line.startswith("[") and "]" in line)   or   ("REF:" in line)    or   (line == "\n") 
-            or   (line.startswith("<c=1>"))  
-            or   (line.startswith("<c=2>")) 
-            or   (line.startswith("<c=3>"))  
-            or   (line.startswith("<b=9>"))  
+            or   (line.startswith("<c="))  
+            or   (line.startswith("<b="))   
+            ): 
             
-            ):   
             pass
+        
         else:
             line = "BD_TRANSLATE_TEXT=" + line
             
@@ -65,7 +65,21 @@ def fix_INI_file(in_INI_filepath, out_INI_filepath):
     bd_logger("Ending OmegaT fix...")
 
 
-#OmegaT fix
-p_in_INI_filepath = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\source\\PSP_USA\\2C238264.ini"
-p_out_INI_filepath = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\source\\PSP_USA\\2C238264_fixed.ini"
-fix_INI_file(p_in_INI_filepath, p_out_INI_filepath)
+def main():
+    
+    ini_arr = [ "2C238264.ini", "2C238276.ini", "7EFCA512.ini" ]
+    ini_fixed_arr = [ "2C238264_fixed.ini", "2C238276_fixed.ini", "7EFCA512_fixed.ini" ]
+    ini_path = "c:\\Users\\Arek\\Spolszczenia\\Silent Hill Shattered Memories\\source\\PSP_USA\\"
+    
+    i = 0
+    for item in ini_arr:
+        ini_in = ini_path + item 
+        ini_out = ini_path + ini_fixed_arr[i]
+        fix_INI_file(ini_in, ini_out)
+        i += 1
+        
+    bd_logger("End of main...")
+    
+    
+    
+main()
