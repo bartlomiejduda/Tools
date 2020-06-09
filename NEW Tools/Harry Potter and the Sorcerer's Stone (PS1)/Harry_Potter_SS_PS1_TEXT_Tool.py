9b3c1d0f8ae4abb46abad10290dde220d942ba5a
bartlomiej.duda <ikskoks@gmail.com>
2020-06-09 11:27:43 +0200

    Harry Potter SS PS1 Tools V2

--- NEW Tools/Harry Potter and the Sorcerer's Stone (PS1)/Harry_Potter_SS_PS1_TEXT_Tool.py
+++ NEW Tools/Harry Potter and the Sorcerer's Stone (PS1)/Harry_Potter_SS_PS1_TEXT_Tool.py
@@ -5,6 +5,7 @@
 
 # Ver    Date        Author
 # v0.1   08.06.2020  Bartlomiej Duda
+# v0.2   09.06.2020  Bartlomiej Duda
 
 
 
@@ -26,6 +27,19 @@ def decode_char(in_str):
         o_ch = ord(ch)
         if o_ch > 95 and o_ch < 123:
             ch = chr(ord(ch)+1)
+        
+        #exceptions?
+        elif o_ch == 167 or o_ch == 169:
+            ch = chr(ord(ch)+74)
+        elif o_ch == 132:
+            ch = chr(ord(ch)+59)
+        elif o_ch == 126:
+            ch = chr(ord(ch)+35)
+        
+        
+        #special chars    
+        elif o_ch > 128:
+            ch = chr(ord(ch)+73)
             
         out_str += ch
     return out_str
@@ -38,29 +52,42 @@ def decode_text(in_FILE_path, out_FILE_path):
     bd_logger("Starting decode_text...")    
     
     in_file = open(in_FILE_path, 'rb')
-    out_file = open(out_FILE_path, 'wt+')
+    out_file = open(out_FILE_path, 'wt+', encoding="utf16")
+    
+    XSPL_begin_offset = 160996
+    in_file.seek(XSPL_begin_offset) #TODO    go to XSPL chunk 
     
     
-    in_file.seek(160996) #TODO    go to XSPL chunk 
-    
+    #header reading
     magic = in_file.read(4)
     chunk_size = struct.unpack('<l', in_file.read(4))[0]
+    supp_langs = in_file.read(4)
+    num_of_strings = struct.unpack('<l', in_file.read(4))[0] * 4
+    unknown = in_file.read(4)
+    print("num_of_strings: " + str(num_of_strings) )
     
-    unknowns = in_file.read(12) #TODO 
     
+    #setting base offset
+    str_base_offset = XSPL_begin_offset + 20 + (num_of_strings * 4)
+    real_offset_arr = []
+    
+    
+    #offset table reading
     offset_arr = []
-    for i in range(184):
+    for i in range(num_of_strings):
         offset = struct.unpack('<l', in_file.read(4))[0]
         offset_arr.append(offset)
+        real_offset_arr.append(str_base_offset + offset)
         
-    offset_arr.append(chunk_size)
+    offset_arr.append(offset_arr[-1] + 2)
     
     
-    for i in range(184):
+    #text reading
+    for i in range(num_of_strings):
         str_size = offset_arr[i+1] - offset_arr[i]
-        s_str = "BD_TRANSLATE_TEXT=" +       decode_char(    in_file.read(str_size).decode("utf16", errors='replace')   )
-        print(s_str)
-        #out_file.write(temp_s_str) #TODO
+        s_str = "BD_TRANSLATE_TEXT=" +       decode_char(    in_file.read(str_size).decode("utf16", errors='replace').replace("\x00", "<NULL>").rstrip("<NULL>")   )
+        print( "real_offset: " + str(real_offset_arr[i]) +  " len: " + str(str_size).ljust(3) + " " + str(i+1) + ")" + s_str)
+        out_file.write(s_str + "\n") #TODO
     
     
     
