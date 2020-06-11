18d20797941fe0eede4ad7c0e25ab2be5716727b
bartlomiej.duda <ikskoks@gmail.com>
2020-06-11 11:16:52 +0200

    Homefront Tools V2

--- NEW Tools/Homefront/Homefront_TEXT_Tool.py
+++ NEW Tools/Homefront/Homefront_TEXT_Tool.py
@@ -5,6 +5,7 @@
 
 # Ver    Date        Author
 # v0.1   11.06.2020  Bartlomiej Duda
+# v0.2   11.06.2020  Bartlomiej Duda
 
 
 
@@ -33,13 +34,32 @@ def convert_text(in_TEXT_path, out_TEXT_path):
     bd_logger("Starting convert_text...")    
     
     in_file = open(in_TEXT_path, 'rb')
-    out_file = open(out_TEXT_path, 'wt+')
+    out_file = open(out_TEXT_path, 'wt+', encoding="utf8")
     
     
-    for i in range(1): #TODO
-        val1 = in_file.read(4)
-        str_len = struct.unpack('<l', xore(in_file.read(4), b'\xFF'))[0] * 2
-        print("str_len: " + str(str_len) )
+    i = 0
+    while 1:
+        i += 1
+        back_offset = in_file.tell()  
+        try:
+            str_len = struct.unpack('<l', xore(in_file.read(4), b'\xFF'))[0] * 2
+        except:
+            bd_logger("End of file. Exiting loop.")
+            break
+        
+        if str_len <= 0:
+            in_file.seek(back_offset)
+            val1_offset = in_file.tell() 
+            val1 = struct.unpack('<l', in_file.read(4))[0]
+            str_len = struct.unpack('<l', xore(in_file.read(4), b'\xFF'))[0] * 2
+            #print( str(i) + ") " + "val1_offset: " + str(val1_offset) + " val1: " + str(val1) )
+        
+        curr_offset = in_file.tell()    
+        b_str = in_file.read(str_len)
+        null = in_file.read(2)
+        s_str = b_str.decode("utf16")
+        #print( str(i) + ") " + "curr_offset: " + str(curr_offset) )
+        out_file.write(s_str + "\n")
     
     
     
@@ -62,6 +82,11 @@ def main():
         in_filepath = "C:\\Users\\Arek\\Desktop\\Homefront Loc\\Coalesced_int.bin"
         out_ini_path = "C:\\Users\\Arek\\Desktop\\Homefront Loc\\Coalesced_int.bin_OUT.ini"
         convert_text(in_filepath, out_ini_path)
+        
+        in_filepath = "C:\\Users\\Arek\\Desktop\\Homefront Loc\\Coalesced_int_full.bin"
+        out_ini_path = "C:\\Users\\Arek\\Desktop\\Homefront Loc\\Coalesced_int_full.bin_OUT.ini"
+        convert_text(in_filepath, out_ini_path)        
+        
         
     else:
         print("Wrong option selected!")
