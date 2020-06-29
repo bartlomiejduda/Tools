40a1bfc7761274625de74189acede3d5bf6e8ab9
bartlomiej.duda <ikskoks@gmail.com>
2020-06-29 13:21:17 +0200

    BDTexView V5

--- NEW Tools/BDTexView/BDTexView.py
+++ NEW Tools/BDTexView/BDTexView.py
@@ -7,10 +7,11 @@
 # v0.2   23.06.2020  Bartlomiej Duda    -
 # v0.3   26.06.2020  Bartlomiej Duda    -
 # v0.4   27.06.2020  Bartlomiej Duda    -
+# v0.5   29.06.2020  Bartlomiej Duda    -
 
 
 
-VERSION_NUM = "v0.4"
+VERSION_NUM = "v0.5"
 
 
 import os
@@ -76,9 +77,14 @@ def about_window(self):
 
 def main():
     
-    #app setting
+    #default app settings
     WINDOW_HEIGHT = 700
     WINDOW_WIDTH = 800
+    
+    #default yellow canvas settings
+    canv_yellow_settings = [ 10,   70,  200,   180,    0          ] 
+                            #x     y    width  height  change_flag    
+    
     root = tk.Tk("BDTexView", "BDTexView")
     root.winfo_toplevel().title("BDTexView " + VERSION_NUM)
     
@@ -138,8 +144,7 @@ def main():
     image = ImageTk.PhotoImage(pilImage)
     
     
-    canv_yellow_settings = [ 10,   70,  180,   180,    0          ] 
-                            #x     y     width  height  change_flag
+
     
     canv_yellow = tk.Canvas(main_frame, bg='yellow')
     canv_yellow.place(x= canv_yellow_settings[0], y= canv_yellow_settings[1], width=canv_yellow_settings[2], height=canv_yellow_settings[3])
@@ -149,28 +154,48 @@ def main():
     
     
     
-    #buttons for manipulating canvas
-    canv_frame = LabelFrame(main_frame, text="Canvas Size", padx=5, pady=5)
-    canv_frame['bg'] = canv_frame.master['bg']
-    canv_frame.place(relx= 0, x=10, rely= 1, y=-120, width=135, height=110)    
-    butt1 = tk.Button(canv_frame, text="Left", command=lambda: change_canvas_width(canv_yellow, canv_yellow_settings, -10) )
+    
+    #INFO
+    canv_info_box = LabelFrame(main_frame, text="Info", padx=5, pady=5)
+    canv_info_box['bg'] = canv_info_box.master['bg']
+    canv_info_box.place(relx= 0, x=150, rely= 1, y=-120, width=135, height=110)   
+    
+    canv_h_label = tk.Label( canv_info_box, text="Canvas height: " + str(canv_yellow_settings[3]), anchor="w")
+    canv_h_label['bg'] = canv_h_label.master['bg']
+    canv_h_label.place(x= 5, y= 0, width=120, height=20)  
+    
+    canv_w_label = tk.Label( canv_info_box, text="Canvas width: " + str(canv_yellow_settings[2]), anchor="w")
+    canv_w_label['bg'] = canv_h_label.master['bg']
+    canv_w_label.place(x= 5, y= 15, width=120, height=20)      
+
+
+    
+        
+    
+    
+    
+    #buttons for manipulating canvas (CANVAS SIZE)
+    canv_size_box = LabelFrame(main_frame, text="Canvas Size", padx=5, pady=5)
+    canv_size_box['bg'] = canv_size_box.master['bg']
+    canv_size_box.place(relx= 0, x=10, rely= 1, y=-120, width=135, height=110)    
+    butt1 = tk.Button(canv_size_box, text="Left", command=lambda: change_canvas_width(canv_w_label, canv_yellow, canv_yellow_settings, -10) )
     butt1.place(x= 10, y= 30, width=40, height=20)    
-    butt2 = tk.Button(canv_frame, text="Right", command=lambda: change_canvas_width(canv_yellow, canv_yellow_settings, 10) )
+    butt2 = tk.Button(canv_size_box, text="Right", command=lambda: change_canvas_width(canv_w_label, canv_yellow, canv_yellow_settings, 10) )
     butt2.place(x= 70, y= 30, width=40, height=20)  
-    butt3 = tk.Button(canv_frame, text="Up", command=lambda: change_canvas_height(canv_yellow, canv_yellow_settings, -10) )
+    butt3 = tk.Button(canv_size_box, text="Up", command=lambda: change_canvas_height(canv_h_label, canv_yellow, canv_yellow_settings, -10) )
     butt3.place(x= 40, y= 5, width=40, height=20)     
-    butt4 = tk.Button(canv_frame, text="Down", command=lambda: change_canvas_height(canv_yellow, canv_yellow_settings, 10) )
-    butt4.place(x= 40, y= 55, width=40, height=20)   
+    butt4 = tk.Button(canv_size_box, text="Down", command=lambda: change_canvas_height(canv_h_label, canv_yellow, canv_yellow_settings, 10) )
+    butt4.place(x= 40, y= 55, width=40, height=20)  
     
+
     
-    
-    #debug buttons
-    canv_frame = LabelFrame(main_frame, text="Debug Options", padx=5, pady=5)
-    canv_frame['bg'] = canv_frame.master['bg']
-    canv_frame.place(relx= 1, x=-210, rely= 1, y=-180, width=200, height=100)    
-    butt1 = tk.Button(canv_frame, text="Open Log", command=lambda: open_log() )
+    #debug buttons (DEBUG OPTIONS)
+    canv_debug_box = LabelFrame(main_frame, text="Debug Options", padx=5, pady=5)
+    canv_debug_box['bg'] = canv_debug_box.master['bg']
+    canv_debug_box.place(relx= 1, x=-210, rely= 1, y=-180, width=200, height=100)    
+    butt1 = tk.Button(canv_debug_box, text="Open Log", command=lambda: open_log() )
     butt1.place(x= 10, y= 10, width=80, height=20)    
-    butt2 = tk.Button(canv_frame, text="File Properties", command=lambda: file_properties() )
+    butt2 = tk.Button(canv_debug_box, text="File Properties", command=lambda: file_properties() )
     butt2.place(x= 10, y= 40, width=90, height=20)      
     
     
@@ -189,7 +214,8 @@ def main():
     menubar.add_cascade(label="Image", menu=imagemenu)    
     
     optionsmenu = tk.Menu(menubar, tearoff=0)
-    optionsmenu.add_command(label="Settings", command=lambda: open_settings_window())
+    optionsmenu.add_command(label="Export Settings", command=lambda: export_settings())
+    optionsmenu.add_command(label="Import Settings", command=lambda: import_settings())
     menubar.add_cascade(label="Options", menu=optionsmenu)
     
     helpmenu = tk.Menu(menubar, tearoff=0)
@@ -201,14 +227,15 @@ def main():
     root.mainloop()
     
  
-def change_canvas_width(in_canvas, in_canvas_settings, step):
+def change_canvas_width(in_w_label, in_canvas, in_canvas_settings, step):
     in_canvas_settings[2] = in_canvas_settings[2] + step
-    in_canvas.place(x= in_canvas_settings[0], y= in_canvas_settings[1], width=in_canvas_settings[2], height=in_canvas_settings[3])   
+    in_canvas.place(x= in_canvas_settings[0], y= in_canvas_settings[1], width=in_canvas_settings[2], height=in_canvas_settings[3])  
+    in_w_label.config(text="Canvas width: " + str(in_canvas_settings[2]) )
     
-def change_canvas_height(in_canvas, in_canvas_settings, step):
+def change_canvas_height(in_h_label, in_canvas, in_canvas_settings, step):
     in_canvas_settings[3] = in_canvas_settings[3] + step
     in_canvas.place(x= in_canvas_settings[0], y= in_canvas_settings[1], width=in_canvas_settings[2], height=in_canvas_settings[3])   
-    
+    in_h_label.config(text="Canvas height: " + str(in_canvas_settings[3]) )
  
     
     
