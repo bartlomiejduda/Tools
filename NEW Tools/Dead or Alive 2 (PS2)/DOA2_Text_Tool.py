26c46887901cfb8d42831ff3c0ef7a2efd4b449b
bartlomiej.duda <ikskoks@gmail.com>
2020-09-12 18:08:01 +0200

    Dead or Alive 2 texts extract

--- NEW Tools/Dead or Alive 2 (PS2)/DOA2_Text_Tool.py
+++ NEW Tools/Dead or Alive 2 (PS2)/DOA2_Text_Tool.py
@@ -5,6 +5,7 @@
 
 # Ver    Date        Author
 # v0.1   11.09.2020  Bartlomiej Duda
+# v0.2   12.09.2020  Bartlomiej Duda
 
 import os
 import sys
@@ -25,7 +26,6 @@ def read_str(in_file):
     while 1:
         ch = struct.unpack('c', in_file.read(1))[0]
         ch_d = ch.decode(main_encoding)
-        #print("ch: " + str(ch))
         if ord(ch_d) != 0:
             out_str += ch
         else:
@@ -95,14 +95,58 @@ def collect_data(in_file_path, out_file_path, out2_file_path):
 
 
     
-def extract_text():
-    pass #TODO
+def extract_text(in_exe_filepath, out_text_filepath):
+    '''
+    Function for extracting english texts
+    '''    
+    bd_logger("Starting extract_text...")    
+    
+    in_file = open(in_exe_filepath, 'rb')
+    out_file = open(out_text_filepath, 'wt+', encoding="windows-1252")
+    eng_data_file = open("strings_eng_data.txt", 'rt')
+    
+    
+    offset_arr = []
+    size_arr = []
+    texts_count = 0
+    for line in eng_data_file: #gathering data for text extraction
+        texts_count += 1
+        line = line.rstrip("\n")
+        
+        off_str = line.split("\t")[0]
+        size_str = line.split("\t")[1]
+        
+        off_i = int(off_str, 16)
+        size_i = int(size_str, 16)
+        
+        offset_arr.append(off_i)
+        size_arr.append(size_i)
+        
+    
+    
+    for i in range(texts_count): #saving texts to file
+        in_file.seek(offset_arr[i])
+        text_str = in_file.read(size_arr[i]).decode("windows-1252")
+        #print(text_str)
+        out_str = str(offset_arr[i]) + "_" + str(size_arr[i]) + "=" + text_str
+        out_file.write(out_str + "\n")
+    
+    
+    
+    
+    eng_data_file.close()
+    in_file.close()
+    out_file.close()
+    bd_logger("Ending extract_text...")        
+    
+    
     
     
 def main():
     
-    main_switch = 1
+    main_switch = 2
     # 1 - text data collect
+    # 2 - text extract
 
     
 
@@ -112,6 +156,10 @@ def main():
         p_out2_filepath = "C:\\Users\\Arek\\Desktop\\DOA2 PS2\\GAME_FILES\\out2.txt"
         collect_data(p_in_filepath, p_out_filepath, p_out2_filepath)
         
+    elif main_switch == 2:
+        p_in_filepath = "C:\\Users\\Arek\\Desktop\\DOA2 PS2\\GAME_FILES\\SLUS_200.71"
+        p_out_filepath = "C:\\Users\\Arek\\Desktop\\DOA2 PS2\\GAME_FILES\\Dead_or_alive_2_ENG_script.ini"
+        extract_text(p_in_filepath, p_out_filepath)
    
     else:
         print("Wrong option selected!")
