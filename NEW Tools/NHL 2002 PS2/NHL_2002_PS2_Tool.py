3d915e0a186ae093b0e8949c0f7072e6906a518a
Bartlomiej Duda <ikskoks@gmail.com>
2021-02-01 01:02:03 +0100

    NHL Tool update

--- NEW Tools/NHL 2002 PS2/NHL_2002_PS2_Tool.py
+++ NEW Tools/NHL 2002 PS2/NHL_2002_PS2_Tool.py
@@ -49,10 +49,7 @@ class BMP_IMG:
     class BMP_INFO_HEADER:
         def __init__(self, in_width, in_height, in_bpp):
             self.info_header_size = 40
-            self.im_width = -1
-            self.im_height = -1
             self.num_of_planes = 1
-            self.bpp = -1
             self.comp_type = 0
             self.comp_im_size = 0
             self.pref_hor_res = 0
@@ -123,6 +120,7 @@ def export_data(in_file_path, out_folder_path):
         magic = struct.unpack("4s", ssh_file.read(4))[0].decode("utf8")
     except:
         bd_logger("Can't read magic! Aborting!")
+        return
         
     if magic not in ("SHPS", "SHPP"):
         bd_logger("It is not supported EA graphics file! Aborting!")
@@ -133,7 +131,7 @@ def export_data(in_file_path, out_folder_path):
     ssh_file.read(4) # directory ID
     
     
-    for i in range(num_of_files):
+    for f_count in range(num_of_files):
         file_name = struct.unpack("4s", ssh_file.read(4))[0].decode("utf8")
         file_offset = struct.unpack("<L", ssh_file.read(4))[0]
 
@@ -142,6 +140,7 @@ def export_data(in_file_path, out_folder_path):
         
         #reading image header
         image_type = struct.unpack("<B", ssh_file.read(1))[0]
+        im_bpp = -1
         block_size = struct.unpack("<L", ssh_file.read(3) + b'\x00')[0] - 16 
         if block_size < 0:
             block_size = 0
@@ -154,55 +153,86 @@ def export_data(in_file_path, out_folder_path):
         pal_data_offset = im_data_offset + block_size
         
 
- 
-        
-        
         print("file_name: " + str(file_name) +
               " image_type: " + str(image_type)
               )
                  
         
-        # reading image data + skew fix
+       
         ssh_file.seek(im_data_offset)
         
-        bmp_data = b'' # SKEW FIX
-        temp_row = b''
-        skew_val = im_width % 4      
-        for i in range(im_height):
-            temp_row = b''
-            for j in range(im_width):
-                pixel = ssh_file.read(1)
-                temp_row += pixel
-            if skew_val == 1:
-                temp_row += b'\x00\x00'
-            elif skew_val == 2:
-                temp_row += b'x\00'
-                
-            row_len = len(temp_row)
-            bmp_data += temp_row
         
-        diff = block_size - im_size_calc
-        if diff > 0:
-            bmp_data += ssh_file.read(diff)
-        
-
-        #reading palette 
-        ssh_file.seek(pal_data_offset)
-        pal_header_data = ssh_file.read(15) # palette header
-        palette_data = b''
-        for i in range(256):
-            pal_entry1 = ssh_file.read(1)
-            pal_entry2 = ssh_file.read(1)
-            pal_entry3 = ssh_file.read(1)
-            pal_entry4 = ssh_file.read(1)
-            palette_data += pal_entry4 + pal_entry3 + pal_entry2 + pal_entry1 # RGBA swap
+        if image_type == 2: # 8-bit skewed image with 256-colors swapped palette
             
+            bmp_data = b'' # SKEW FIX
+            temp_row = b''
+            skew_val = im_width % 4      
+            for i in range(im_height):
+                temp_row = b''
+                for j in range(im_width):
+                    pixel = ssh_file.read(1)
+                    temp_row += pixel
+                if skew_val == 1:
+                    temp_row += b'\x00\x00'
+                elif skew_val == 2:
+                    temp_row += b'x\00'
+                    
+                row_len = len(temp_row)
+                bmp_data += temp_row
+            
+            diff = block_size - im_size_calc
+            bmp_data += ssh_file.read(diff)
+            
+    
+            #reading palette 
+            ssh_file.seek(pal_data_offset)
+            pal_header_data = ssh_file.read(15) # palette header
+            palette_data = b''
+            for i in range(256):
+                pal_entry1 = ssh_file.read(1)
+                pal_entry2 = ssh_file.read(1)
+                pal_entry3 = ssh_file.read(1)
+                pal_entry4 = ssh_file.read(1)
+                palette_data += pal_entry4 + pal_entry3 + pal_entry2 + pal_entry1 # RGBA swap
+                
+            im_bpp = 8
+            
+            
+        elif image_type == 64: # 4-bit image with 16-colors palette  
+            im_size = int((im_width * im_height) / 2)
+            bmp_data = ssh_file.read(im_size)
+            
+            ssh_file.read(15) # palette header 
+            palette_data = ssh_file.read(16)
+            im_bpp = 4
+            
+            
+        elif image_type == 65: # 8-bit image with 256-colors palette (15 bits per color in palette)
+            im_size = im_width * im_height
+            bmp_data = ssh_file.read(im_size)
+            ssh_file.read(16) # palette header
+            palette_data = ssh_file.read(512)  # TODO - convert this 15-bit palette 
+                                               # to 32-bit for BMP output
+            im_bpp = 8           
+                
+        elif image_type == 66:  # 16-bit image (no palette)
+            im_size = im_width * im_height * 2
+            bmp_data = ssh_file.read(im_size)
+            palette_data = b''
+            im_bpp = 16
+            
+            
+        else:
+            bd_logger("Unsupported image type " + str(image_type) + "! Skipping!")
+            ssh_file.seek(back_offset)
+            continue
+                      
 
 
         # writing bmp
-        bmp_object = BMP_IMG(im_width, im_height, 8, bmp_data, palette_data)
+        bmp_object = BMP_IMG(im_width, im_height, im_bpp, bmp_data, palette_data)
         bmp_file_data = bmp_object.get_bmp_file_data()
-        out_file_path = out_folder_path + file_name.replace(">", "0") + ".bmp"
+        out_file_path = out_folder_path + file_name.replace(">", "0") + "_" + str(f_count+1) + ".bmp"
         out_file = open(out_file_path, "wb+")  
         out_file.write(bmp_file_data)
         out_file.close()
@@ -238,6 +268,9 @@ def main():
     if main_switch == 1:
         p_in_file_path = "C:\\Users\\Arek\\Desktop\\EA SAMPLES\\NHL 2002 SSH\\awards.ssh"
         p_out_folder_path = "C:\\Users\\Arek\\Desktop\\EA SAMPLES\\NHL 2002 SSH\\awards.ssh_OUT\\"
+        
+        #p_in_file_path = "C:\\Users\\Arek\\Desktop\\EA SAMPLES\\NBA Live 97 PS1\\ZLOADSCR.PSH"
+        #p_out_folder_path = "C:\\Users\\Arek\\Desktop\\EA SAMPLES\\NBA Live 97 PS1\\ZLOADSCR.PSH_OUT\\"        
         export_data(p_in_file_path, p_out_folder_path)
         
     else:
