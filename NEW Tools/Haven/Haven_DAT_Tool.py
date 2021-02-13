9915d8fa66203923a9928a2f4031e98b952b286b
Bartlomiej Duda <ikskoks@gmail.com>
2021-02-13 22:39:04 +0100

    Haven DAT Tool update

--- NEW Tools/Haven/Haven_DAT_Tool.py
+++ NEW Tools/Haven/Haven_DAT_Tool.py
@@ -10,7 +10,7 @@ License: GPL-3.0 License
 # It should be used with Haven
 
 # Ver    Date        Author               Comment
-# v0.1   14.01.2021  Bartlomiej Duda      -
+# v0.1   13.02.2021  Bartlomiej Duda      -
 
 import os
 import sys
@@ -30,16 +30,17 @@ def calculate_padding_len(in_len):
     padding_val = (8 - (in_len % 8)) % 8
     return padding_val
 
-def calculate_padding_len_v2(in_len):
-    mod_res = int(in_len % 4)
-    if mod_res == 0:
-        return mod_res
-    else:
-        res = 4 - mod_res
-        return res  
+#def calculate_padding_len_v2(in_len):
+    #mod_res = int(in_len % 4)
+    #if mod_res == 0:
+        #return mod_res
+    #else:
+        #res = 4 - mod_res
+        #return res  
  
 def calculate_padding_len_v3(in_len):
-    padding_val = (4 - (in_len % 4)) % 4
+    div = 4
+    padding_val = (div - (in_len % div)) % div
     return padding_val   
     
 
@@ -52,26 +53,6 @@ def read_nulls(in_file):
             return
 
 
-#def read_person_string(in_file):
-    #back_offset = in_file.tell()
-    #try: # this is only a workaround!
-        #p_string_ID = struct.unpack("<L", in_file.read(4))[0]
-        #if p_string_ID == 1:
-            #person_str_len = struct.unpack("<L", in_file.read(4))[0]
-            #person_str = in_file.read(person_str_len)
-        #elif p_string_ID == 3:
-            #person_str_len = p_string_ID
-            #person_str = in_file.read(person_str_len)
-        #else:
-            #raise
-        
-        #print("person_str: " + str(person_str))
-        
-    #except:
-        #in_file.seek(back_offset)
-        #return
-
-
 def export_data(in_file_path, out_folder_path):
     '''
     Function for exporting data from DAT files
@@ -82,19 +63,39 @@ def export_data(in_file_path, out_folder_path):
         os.makedirs(out_folder_path)      
     
     dat_file = open(in_file_path, 'rb')
+    out_file = open(out_folder_path + "out.txt", "wb+")
     
-    #num_of_entries = struct.unpack("<L", dat_file.read(4))[0]
-    #print(num_of_entries)
     
     for i in range(99999999):
         
-        #read_person_string(dat_file)
-        #read_nulls(dat_file)
         
         person_offset = dat_file.tell()
+        
+        
+        
         person_string = struct.unpack("4s", dat_file.read(4))[0].decode("utf8").rstrip('\x00')
         if person_string == "":
             person_string = "None"
+            
+            
+        elif person_string in ('\x05'): # workaround!
+            dat_file.seek( dat_file.tell() - 4)
+            person_string_len = struct.unpack("<L", dat_file.read(4))[0]
+            person_string = dat_file.read(person_string_len).decode("utf8")
+            padding_len = calculate_padding_len(person_string_len)
+            dat_file.read(padding_len)             
+            
+            
+        elif person_string in ('\x02'): # workaround!
+            dat_file.read(4)
+            person_string = "Yu"
+        elif person_string in ('\x03'): # workaround!
+            dat_file.read(4)
+            person_string = "Kay"
+        elif person_string in ("Eren", "Horn", "Ozia"): # workaround!
+            dat_file.seek(person_offset)
+            person_string = struct.unpack("8s", dat_file.read(8))[0].decode("utf8").rstrip('\x00')
+            
         print("PERSON: " + str(person_string))
         
         
@@ -111,36 +112,21 @@ def export_data(in_file_path, out_folder_path):
         default_str_len = struct.unpack("<L", dat_file.read(4))[0]
         default_str = dat_file.read(default_str_len)
         
-        padding_len = calculate_padding_len(default_str_len)
+        padding_len = calculate_padding_len_v3(default_str_len)
         dat_file.read(padding_len)   
-        #read_nulls(dat_file)
-        #dat_file.read(8)
         
         num_of_languages = struct.unpack("<L", dat_file.read(4))[0]
         
-        if num_of_languages == 0:
-            #read_nulls(dat_file)
-            dat_file.read(4)
-            continue
-            #num_of_subentries = struct.unpack("<L", dat_file.read(4))[0]
-            
-            #for k in range(num_of_subentries):
-                #len_of_str = struct.unpack("<L", dat_file.read(4))[0]
-                #Yu_str = dat_file.read(len_of_str)
-                #padding_len = calculate_padding_len(len_of_str)
-                #dat_file.read(padding_len)    
-                
-            ##dat_file.read(4)
-            #read_nulls(dat_file)
-        
-        #else:
+        #if num_of_languages == 0:
+            #dat_file.read(4)
+            #continue
+
         
         
         for j in range(num_of_languages):
 
             lang_ID_length = struct.unpack("<L", dat_file.read(4))[0]
             lang_ID = dat_file.read(lang_ID_length)  
-            #print(lang_ID) = struct.unpack <
             
             padding_len = calculate_padding_len_v3(lang_ID_length)
             dat_file.read(padding_len)      
@@ -148,34 +134,40 @@ def export_data(in_file_path, out_folder_path):
             string_length = struct.unpack("<L", dat_file.read(4))[0]
             str_offset = dat_file.tell()
             
-            if str_offset > 167915:
+            if str_offset > 4375884 - 1:
                 a = 5 # debug
             
             
             if string_length == 0:
-                back_offset = dat_file.tell()
-                temp = struct.unpack("<L", dat_file.read(4))[0]
-                if temp > 0:
-                    dat_file.seek(back_offset)
-                #read_nulls(dat_file)
                 out_string = ""
             else:
                 out_string = dat_file.read(string_length).decode("utf8")
             
             padding_len = calculate_padding_len_v3(string_length) # 2
             dat_file.read(padding_len)  
-            #read_nulls(dat_file)
-            #dat_file.seek(dat_file.tell() - 4)
+
             
             end_offset = dat_file.tell()
             
-            print("str_offset: " + str(str_offset) + 
+            print(str(lang_ID.decode("utf8")) + "= " + 
+                  " str_offset: " + str(str_offset) + 
                   " str_len: " + str(string_length) +
                   " " + str(out_string))
                 
-            #dat_file.read(4)
+
+            
+            
+            
+        num_of_something = struct.unpack("<L", dat_file.read(4))[0]
+        
+        for i in range(num_of_something):
+            some_value = dat_file.read(4)
     
    
+    
+    
+    
+    
     
     dat_file.close()
     bd_logger("Ending export_data...")    
