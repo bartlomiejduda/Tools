8fc0ecc0f6e0bcbb4672b4fd9b5db0fe4daddc6e
Bartlomiej Duda <ikskoks@gmail.com>
2022-06-05 22:10:13 +0200

    Updated argparse template

--- NEW Tools/TEMPLATES/template_argparse.py
+++ NEW Tools/TEMPLATES/template_argparse.py
@@ -9,16 +9,38 @@ License: GPL-3.0 License
 # v0.1   28.05.2022  Bartlomiej Duda      -
 
 
-# This program is for ...
 
 import os
 import sys
 import argparse
 from typing import Optional
-import xmltodict
 from logger import get_logger
 
 logger = get_logger(__name__)
+
+VERSION_NUM = "v0.1"
+EXE_FILE_NAME = f"some_game_text_tool_{VERSION_NUM}.exe"
+PROGRAM_NAME = f'Some Game Text Tool {VERSION_NUM}'
+
+def check_file(in_file_path, create_dirs=False):
+    if not os.path.isfile(in_file_path):
+        return "NOT_FILE_ERROR", "This is not a valid input file path!"
+
+    in_file_extension = in_file_path.split(".")[-1]
+    if in_file_extension.upper() != "XML":
+        return "NOT_XML_ERROR", f"{in_file_path} is not a valid XML file!"
+
+    if create_dirs:
+        if not os.path.exists(os.path.dirname(in_file_path)):
+            try:
+                os.makedirs(os.path.dirname(in_file_path))
+            except FileNotFoundError:
+                return "CANT_CREATE_DIR_ERROR", "Can't create output directory!"
+
+
+
+def check_directory(in_directory):
+    pass # TODO
 
 
 def export_data(in_file_path: str, out_file_path: str) -> Optional[tuple]:
@@ -27,48 +49,53 @@ def export_data(in_file_path: str, out_file_path: str) -> Optional[tuple]:
     """
     logger.info("Starting export_data...")
 
-    if not os.path.isfile(in_file_path):
-        return "NOT_FILE_ERROR", "This is not a valid input file path!"
+    code, status = check_file(in_file_path)
+    if code != "OK":
+        return code, status
 
-    in_file_extension = in_file_path.split(".")[-1]
-    if in_file_extension.upper() != "XML":
-        return "NOT_XML_ERROR", f"{in_file_path} is not a valid XML file!"
-
-    if not os.path.exists(os.path.dirname(out_file_path)):
-        try:
-            os.makedirs(os.path.dirname(out_file_path))
-        except FileNotFoundError:
-            return "CANT_CREATE_DIR_ERROR", "Can't create output directory!"
-
-    out_file_extension = out_file_path.split(".")[-1]
-    if out_file_extension.upper() != "INI":
-        return "NOT_INI_ERROR", f"{out_file_path} is not a valid INI file!"
+    code, status = check_file(out_file_path)
+    if code != "OK":
+        return code, status
 
     xml_file = open(in_file_path, 'rt', encoding="utf8")
     ini_file = open(out_file_path, 'wt', encoding="utf8")
 
-    xml_data = xml_file.read()
-    xml_dict = xmltodict.parse(xml_data)
 
-   
 
     ini_file.close()
     xml_file.close()
+    logger.info(f'File {out_file_path} has been saved.')
     logger.info("Ending export_data...")
     return "OK", ""
 
 
-def import_data(in_file_path: str, out_folder_path: str) -> Optional[tuple]:
-    return "IMPORT_NOT_SUPPORTED", "Import is not supported yet!"
+def import_data(xml_file_path: str, ini_file_path: str, new_xml_file_path) -> Optional[tuple]:
+    """
+    Function for importing data to XML files
+    """
+    logger.info("Starting import_data...")
+
+
+
+    xml_file = open(xml_file_path, 'rt', encoding="utf8")
+    ini_file = open(ini_file_path, 'rt', encoding="utf8")
+    new_xml_file = open(new_xml_file_path, 'wt', encoding="utf8")
+
+
+    logger.info("Ending import_data...")
+    return "OK", ""
 
 
 def main():
     """
     Main function of this program.
     """
-    parser = argparse.ArgumentParser(prog="satellite_reign_text_tool.exe", description='TEMPLATE Tool v1.0')
-    parser.add_argument('-e', '--ext', type=str, nargs="+", required=False, help='Extract data')
-    parser.add_argument('-i', '--imp', type=str, nargs="+", required=False, help='Import data')
+    parser = argparse.ArgumentParser(prog=EXE_FILE_NAME,
+                                     description=PROGRAM_NAME)
+    parser.add_argument('-e', '--ext', metavar=('<xml_file_path>', '<ini_file_path>'),
+                        type=str, nargs=2, required=False, help='Extract data (convert XML to INI)')
+    parser.add_argument('-i', '--imp', metavar=('<xml_file_path>', '<ini_file_path>', '<new_xml_path>'),
+                        type=str, nargs=3, required=False, help='Import data (convert INI to XML)')
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -77,17 +104,17 @@ def main():
     args = parser.parse_args()
 
     if args.ext is not None:
-        export_result = export_data(args.ext[0], args.ext[1])
-        if export_result[0] != "OK":
-            logger.error(f"{export_result[0]}: {export_result[1]}")
+        code, status = export_data(args.ext[0], args.ext[1])
+        if code != "OK":
+            logger.error(f"{code}: {status}")
             sys.exit(-1)
     elif args.imp is not None:
-        import_result = import_data(args.imp[0], args.imp[1])
-        if import_result[0] != "OK":
-            logger.error(f"{import_result[0]}: {import_result[1]}")
+        code, status = import_data(args.imp[0], args.imp[1], args.imp[2])
+        if code != "OK":
+            logger.error(f"{code}: {status}")
             sys.exit(-2)
 
-    logger.info("End of main... Program has been executed successfully.")
+    logger.info("End of main... Program has been executed successfully!")
     sys.exit(0)
 
 
