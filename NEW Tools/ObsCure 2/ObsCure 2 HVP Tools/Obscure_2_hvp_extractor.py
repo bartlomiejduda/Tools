4c5a7c0e29496208695f63b363058c5bb1744029
Bartlomiej Duda <ikskoks@gmail.com>
2022-12-06 00:19:30 +0100

    Update for Obscure 2 HVP Tools

--- NEW Tools/ObsCure 2/ObsCure 2 HVP Tools/Obscure_2_hvp_extractor.py
+++ NEW Tools/ObsCure 2/ObsCure 2 HVP Tools/Obscure_2_hvp_extractor.py
@@ -7,13 +7,18 @@ License: GPL-3.0 License
 
 # Ver    Date        Author               Comment
 # v0.1   04.12.2022  Bartlomiej Duda      -
+from dataclasses import dataclass
+from typing import List, Optional
 
-
+from reversebox.common.common import convert_int_to_hex_string
 from reversebox.io_files.file_handler import FileHandler
 print("Starting HVP extract script...")
 
-
-hvp_path = "C:\\Users\\Lenovo\\Desktop\\Obscure_2_RESEARCH\\cachpack.hvp"
+# cachpack.hvp
+# kinepack.hvp
+# datapack.hvp
+# loadpack.hvp
+hvp_path = "C:\\GRY\\Obscure 2\\loadpack.hvp"
 hvp_handler = FileHandler(hvp_path, "rb")
 
 hvp_handler.open()
@@ -29,11 +34,51 @@ number_of_entries = hvp_handler.read_uint32()
 directory_crc32 = hvp_handler.read_uint32()
 
 
+@dataclass
+class HashCleanListObject:
+    crc: int
+    path_length: int
+    file_path: str
+
+
+all_hashes_list: List[HashCleanListObject] = []
+
+
+# read hash lists
+hash_list_file = open("obscure_2_hash_clean_list.txt", "rt")
+for line in hash_list_file:
+    if line.startswith("#"):
+        continue
+
+    crc_value, len_value, path_value = line.split("|||")
+    all_hashes_list.append(HashCleanListObject(
+        crc=int(crc_value, 16),
+        path_length=int(len_value),
+        file_path=str(path_value).rstrip('\n')
+    ))
+
+
 # read directory
+#hvp_handler.change_endianess("big")
 for i in range(number_of_entries):
     crc_hash = hvp_handler.read_uint32()
-    # TODO
+    crc_hash_hex = convert_int_to_hex_string(crc_hash)
+    dummy = hvp_handler.read_bytes(20)
+
+    matched_filename: Optional[str] = None
+    full_path: Optional[str] = None
+    for hash_obj in all_hashes_list:
+        if hash_obj.crc == crc_hash:
+            matched_filename = hash_obj.file_path[0:hash_obj.path_length]
+            full_path = hash_obj.file_path
+
+    print("hvp_hash=", crc_hash,
+          "\thvp_hash_str=", crc_hash_hex,
+          "\tmatched_filename=", matched_filename,
+          #"\tfull_path=", full_path
+          )
 
 
+# TODO
 
-print("Export script finished!")  # TODO - replace with logging
+print("Export script finished!")
