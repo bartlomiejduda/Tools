eb90e9e5c160a0f3264079b013cc99ee9bf38d3e
Bartlomiej Duda <ikskoks@gmail.com>
2023-05-14 12:03:31 +0200

    Update for Tail Concerto Tools

--- NEW Tools/Tail Concerto/Tail Concerto Tools/Tail_Concerto_Translation_Tool_main.py
+++ NEW Tools/Tail Concerto/Tail Concerto Tools/Tail_Concerto_Translation_Tool_main.py
@@ -11,13 +11,16 @@ License: GPL-3.0 License
 # v0.3   25.04.2023  Bartlomiej Duda      Add more entries
 # v0.4   01.05.2023  Bartlomiej Duda      Add more entries, update ReverseBox, add character mapping
 # v0.5   01.05.2023  Bartlomiej Duda      Add more entries, List reverse
-
+# v0.6   09.05.2023  Bartlomiej Duda      Update to ReverseBox 0.6.0
+import logging
 from typing import List
 
 from reversebox.common.logger import get_logger
-from reversebox.io_files.translation_text_handler import TranslationTextHandler, TranslationEntry
+from reversebox.io_files.mod_handler import ModHandler
+from reversebox.io_files.translation_text_handler import TranslationTextHandler, TranslationEntry, \
+    windows_1250_pl_no_accents_character_mapping, check_translation_entries
 
-from character_mapping import tail_concerto_import_transform
+from mod_memory import mod_memory
 from translation_memory import translation_memory
 
 logger = get_logger(__name__)
@@ -27,8 +30,10 @@ logger = get_logger(__name__)
 # Read README file for more details
 bin_file_path: str = "C:\\EMULACJA\\AA_GRY_PS1\\Tail Concerto\\OUT\\DATA.BIN"
 po_file_path: str = "C:\\EMULACJA\\AA_GRY_PS1\\Tail Concerto\\OUT\\DATA.BIN.po"
+out_file_path: str = "C:\\EMULACJA\\AA_GRY_PS1\\Tail Concerto\\OUT\\ModHandler_OUT\\"
 
-option: int = 1     # 1 - export text   /   2 - import text
+option: int = 3     # 1 - export text   /   2 - import text
+                    # 3 - export files
 
 
 def get_datetime_string() -> str:
@@ -39,46 +44,10 @@ def get_tail_concerto_encoding() -> str:
     return "windows-1250"
 
 
-def generate_translation_entries(txt_file_path: str, text_key: str = "text_to_translate") -> bool:  # TODO - move it to reversebox
-    """
-    This function should be used along with "strings" program from SysInternals https://learn.microsoft.com/en-us/sysinternals/downloads/strings
-    First, you should generate text dump using command "strings64.exe -o <binary_file_path> > <output_path>.
-    Then you should search for text entries in the dump and copy them to some output file e.g. "entries.txt"
-    Then you just need to run this function with your "entries.txt" as an input parameter for this function.
-    """
-    try:
-        txt_file = open(txt_file_path, "rt")
-    except Exception as error:
-        logger.error(f"Error with opening file: {error}")
-        return False
-
-    for line in txt_file:
-        line = line.strip()
-        offset = line.split(":")[0]
-        text = line.split(":")[-1]
-        output_entry = f"\tTranslationEntry(text_offset={offset}, text_export_length={len(text)}, text_key=\"{text_key}\"),"
-        print(output_entry)
-
-    return True
-
-
-def check_translation_entries(translation_memory_to_check: List[TranslationEntry]) -> bool:
-    """
-    Default function for checking if entries in Translation Memory are correct.
-    """
-    check_offsets_list: List[int] = []
-
-    for translation_entry in translation_memory_to_check:
-        if translation_entry.text_offset in check_offsets_list:
-            logger.error(f"Duplicated text_offset: {translation_entry.text_offset}")
-            return False
-
-        if translation_entry.text_import_length and translation_entry.text_import_length < translation_entry.text_export_length:
-            logger.error(f"Import length is lower than export length for entry with offset {translation_entry.text_offset}")
-            return False
-
-        check_offsets_list.append(translation_entry.text_offset)
-    return True
+def tail_concerto_import_transform(input_bytes: bytes) -> bytes:
+    for key, value in windows_1250_pl_no_accents_character_mapping.items():
+        input_bytes = input_bytes.replace(key, value)
+    return input_bytes
 
 
 def main():
@@ -91,6 +60,12 @@ def main():
             global_import_function=tail_concerto_import_transform,
         )
 
+    mod_handler = ModHandler(
+        mod_memory=mod_memory,
+        archive_file_path=bin_file_path,
+        log_level=logging.INFO
+    )
+
     if option == 1 and translation_handler.export_all_text(po_file_path, creation_date_string=get_datetime_string(),
                                                            revision_date_string=get_datetime_string(),
                                                            encoding=get_tail_concerto_encoding()):
@@ -98,6 +73,8 @@ def main():
     elif option == 2 and translation_handler.import_all_text(po_file_path, create_backup_file=False,
                                                              encoding=get_tail_concerto_encoding()):
         logger.info("Text imported successfully!")
+    elif option == 3 and mod_handler.export_all_files(out_file_path):
+        logger.info("Game data exported successfully!")
     else:
         logger.error("Wrong option or some error occurred! See the logs for more details.")
 
---
+++ NEW Tools/Tail Concerto/Tail Concerto Tools/mod_memory.py
@@ -0,0 +1,76 @@
+from typing import List
+
+from reversebox.io_files.mod_handler import ModEntry
+
+mod_memory: List[ModEntry] = [
+    ModEntry(file_offset=0x04, file_size=3360, file_relative_path="fonts\\font1.tim"),  # 1
+    ModEntry(file_offset=0x23CD0, file_size=8128, file_relative_path="game\\police.tim"),  # 63
+    ModEntry(file_offset=0x66034, file_size=115232, file_relative_path="menu\\ps1_gamepad.tim"),  # 70
+    ModEntry(file_offset=0x9FE40, file_size=3104, file_relative_path="menu\\exit_button.tim"),  # 87
+    ModEntry(file_offset=0x49F958, file_size=229396, file_relative_path="game\\fin.tim"),  # 109
+    ModEntry(file_offset=0x505004, file_size=21024, file_relative_path="menu\\menu_buttons.tim"),  # 115
+    ModEntry(file_offset=0x50A228, file_size=3104, file_relative_path="menu\\default_button.tim"),  # 116
+    ModEntry(file_offset=0x50AE4C, file_size=3104, file_relative_path="menu\\exit_button.tim"),  # 117
+    ModEntry(file_offset=0x50BA70, file_size=75536, file_relative_path="menu\\ps1_gamepad_02.tim"),  # 118
+    ModEntry(file_offset=0x51E804, file_size=8864, file_relative_path="menu\\sound_volume.tim"),  # 119
+    ModEntry(file_offset=0x520AA8, file_size=8864, file_relative_path="menu\\mode.tim"),  # 120
+    ModEntry(file_offset=0x524FF0, file_size=6944, file_relative_path="menu\\sound_configuration.tim"),  # 122
+    ModEntry(file_offset=0x526B14, file_size=8864, file_relative_path="menu\\max.tim"),  # 123
+    ModEntry(file_offset=0x528DB8, file_size=2784, file_relative_path="menu\\stereo.tim"),  # 124
+    ModEntry(file_offset=0x52989C, file_size=2784, file_relative_path="menu\\mono.tim"),  # 125
+    ModEntry(file_offset=0x52A380, file_size=2784, file_relative_path="menu\\stereo2.tim"),  # 126
+    ModEntry(file_offset=0x52AE64, file_size=2784, file_relative_path="menu\\mono2.tim"),  # 127
+    ModEntry(file_offset=0x52BD0C, file_size=3104, file_relative_path="menu\\exit.tim"),  # 129
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),  # 130
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+]
