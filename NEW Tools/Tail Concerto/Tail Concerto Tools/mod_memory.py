36b6d2ddf6354e8e5886327e268b4d8f6aec5a4d
Bartlomiej Duda <ikskoks@gmail.com>
2023-05-14 13:02:42 +0200

    Update for Tail Concerto Tools

--- NEW Tools/Tail Concerto/Tail Concerto Tools/mod_memory.py
+++ NEW Tools/Tail Concerto/Tail Concerto Tools/mod_memory.py
@@ -21,56 +21,14 @@ mod_memory: List[ModEntry] = [
     ModEntry(file_offset=0x52A380, file_size=2784, file_relative_path="menu\\stereo2.tim"),  # 126
     ModEntry(file_offset=0x52AE64, file_size=2784, file_relative_path="menu\\mono2.tim"),  # 127
     ModEntry(file_offset=0x52BD0C, file_size=3104, file_relative_path="menu\\exit.tim"),  # 129
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),  # 130
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
-    ModEntry(file_offset=0x00, file_size=111, file_relative_path="fonts\\aaa.tim"),
+    ModEntry(file_offset=0x52C930, file_size=8864, file_relative_path="menu\\sfx_volume.tim"),  # 130
+    ModEntry(file_offset=0x5311A4, file_size=15424, file_relative_path="game\\locations.tim"),  # 131
+    ModEntry(file_offset=0x575298, file_size=7456, file_relative_path="map\\archeonis.tim"),  # 136
+    ModEntry(file_offset=0x576FBC, file_size=8608, file_relative_path="map\\factory.tim"),  # 137
+    ModEntry(file_offset=0x579160, file_size=9056, file_relative_path="map\\fortress.tim"),  # 138
+    ModEntry(file_offset=0x70DE08, file_size=2112, file_relative_path="buildings\\219_seamus_estate.tim"),  # 329
+    ModEntry(file_offset=0x70E64C, file_size=2112, file_relative_path="buildings\\view_ocean_mine_company.tim"),  # 330
+    ModEntry(file_offset=0x12090EC, file_size=1984, file_relative_path="game\\target_start.tim"),  # 1203
+    ModEntry(file_offset=0x12098B0, file_size=2368, file_relative_path="game\\target.tim"),  # 1204
+    ModEntry(file_offset=0x1D95048, file_size=2112, file_relative_path="game\\book.tim"),  # 1907
 ]
