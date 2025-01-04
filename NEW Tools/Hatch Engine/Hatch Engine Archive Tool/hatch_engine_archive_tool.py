0d4c158cff1fc93e9fbfd4c0759abc6546ce9485
Bartlomiej Duda <ikskoks@gmail.com>
2025-01-04 17:53:11 +0100

    Update Hatch Engine Tools

--- NEW Tools/Hatch Engine/Hatch Engine Archive Tool/hatch_engine_archive_tool.py
+++ NEW Tools/Hatch Engine/Hatch Engine Archive Tool/hatch_engine_archive_tool.py
@@ -10,21 +10,13 @@ from typing import Optional
 
 from reversebox.common.logger import get_logger
 from reversebox.crc.crc32_iso_hdlc import CRC32Handler
+from reversebox.io_files.bytes_helper_functions import set_uint32
 from reversebox.io_files.file_handler import FileHandler
 
 
 logger = get_logger(__name__)
 
-
-# # Index entry from hatch archive
-# @dataclass
-# class IndexEntry:
-#     crc32: int
-#     file_offset: int
-#     file_uncompressed_size: int
-#     data_flag: int
-#     file_compressed_size: int
-#     file_name: Optional[str] = None
+crc_handler = CRC32Handler()
 
 
 # Entry for filename from TXT file and for calculated CRC value
@@ -34,7 +26,68 @@ class KnownCRCEntry:
     file_name: str
 
 
-def export_data(hatch_file_path: str, filenames_list_file_path: str) -> bool:
+def decrypt_hatch_data(f_data: bytes, filename: str, f_size: int) -> bytes:
+    keyA: bytearray = bytearray(16)
+    keyB: bytearray = bytearray(16)
+    decrypted_data: bytearray = bytearray(f_data)
+
+    filename_hash: int = crc_handler.calculate_crc32(filename.encode("ascii"))
+    encoded_filename_hash: bytes = set_uint32(filename_hash, "<")
+    size_hash: int = crc_handler.calculate_crc32(f_data)
+    encoded_size_hash: bytes = set_uint32(size_hash, "<")
+
+    keyA[0:4] = encoded_filename_hash
+    keyA[4:8] = encoded_filename_hash
+    keyA[8:12] = encoded_filename_hash
+    keyA[12:16] = encoded_filename_hash
+
+    keyB[0:4] = encoded_size_hash
+    keyB[4:8] = encoded_size_hash
+    keyB[8:12] = encoded_size_hash
+    keyB[12:16] = encoded_size_hash
+
+    swap_nibbles: int = 0
+    index_keyA: int = 0
+    index_keyB: int = 8
+
+    xor_value = (f_size >> 2) & 0x7F
+
+    for x in range(f_size):
+        temp = decrypted_data[x]
+
+        temp ^= xor_value ^ keyB[index_keyB]
+        index_keyB += 1
+
+        if swap_nibbles:
+            temp = ((temp & 0x0F) << 4) | ((temp & 0xF0) >> 4)
+
+        temp ^= keyA[index_keyA]
+        index_keyA += 1
+
+        decrypted_data[x] = temp
+
+        if index_keyA <= 15:
+            if index_keyB > 12:
+                index_keyB = 0
+                swap_nibbles ^= 1
+        elif index_keyB <= 8:
+            index_keyA = 0
+            swap_nibbles ^= 1
+        else:
+            xor_value = (xor_value + 2) & 0x7F
+            if swap_nibbles:
+                swap_nibbles = False
+                index_keyA = xor_value % 7
+                index_keyB = (xor_value % 12) + 2
+            else:
+                swap_nibbles = True
+                index_keyA = (xor_value % 12) + 3
+                index_keyB = xor_value % 7
+
+    return f_data
+
+
+def export_data(hatch_file_path: str, filenames_list_file_path: str, output_directory_path: str) -> bool:
     """
     Function for exporting data
     """
@@ -42,9 +95,7 @@ def export_data(hatch_file_path: str, filenames_list_file_path: str) -> bool:
 
     # Reading names from TXT file
     filenames_file = open(filenames_list_file_path, "rt")
-    # index_entries_list: list[IndexEntry] = []
     known_crc_entries_list: list[KnownCRCEntry] = []
-    crc_handler = CRC32Handler()
 
     for line in filenames_file:
         f_filename: str = line.strip()
@@ -73,36 +124,46 @@ def export_data(hatch_file_path: str, filenames_list_file_path: str) -> bool:
         f_data_flag: int = hatch_file.read_uint32()
         f_compressed_size: int = hatch_file.read_uint64()
         f_filename: Optional[str] = f"Unknown_Files/file{i}.bin"
+        f_filename_known_flag: bool = False
         back_offset: int = hatch_file.get_position()
 
         for known_crc_entry in known_crc_entries_list:
             if known_crc_entry.crc32 == f_crc:
                 f_filename = known_crc_entry.file_name
+                f_filename_known_flag = True
 
         logger.info(f'{i}) {f_filename}')
 
         hatch_file.seek(f_offset)
 
+        # Decompression logic
         if f_compressed_size != f_uncompressed_size:
             f_data: bytes = hatch_file.read_bytes(f_compressed_size)
             f_data = zlib.decompress(f_data)
         else:
             f_data: bytes = hatch_file.read_bytes(f_uncompressed_size)
 
+        # Decryption logic
+        if f_data_flag == 2 and f_filename_known_flag:
+            f_data = decrypt_hatch_data(f_data, f_filename, f_uncompressed_size)
+            pass
 
-        # TODO
+        # Save data logic
+        absolute_file_path: str = os.path.join(output_directory_path, f_filename.replace("/", "\\"))
+        absolute_dir_path: str = os.path.dirname(absolute_file_path)
+
+        if not os.path.exists(absolute_dir_path):
+            try:
+                os.makedirs(absolute_dir_path)
+            except FileNotFoundError as error:
+                logger.error(f"Can't create output directory! Exiting! Error: {error}")
+                exit(1)
+
+        output_file = open(absolute_file_path, "wb")
+        output_file.write(f_data)
+        output_file.close()
 
         hatch_file.seek(back_offset)
-
-        # index_entry: IndexEntry = IndexEntry(
-        #     crc32=f_crc,
-        #     file_offset=f_offset,
-        #     file_uncompressed_size=f_uncompressed_size,
-        #     data_flag=f_data_flag,
-        #     file_compressed_size=f_compressed_size,
-        #     file_name=f_filename
-        # )
-        # index_entries_list.append(index_entry)
 
     hatch_file.close()
     logger.info("Ending export data...")
@@ -113,9 +174,10 @@ def main():
     """
     Main function of this program.
     """
-    hatch_file_path = os.environ['HATCH_FILE_PATH']
-    filenames_list_file_path = os.environ['FILENAMES_LIST_FILE_PATH']
-    export_data(hatch_file_path, filenames_list_file_path)
+    hatch_file_path: str = os.environ['HATCH_FILE_PATH']
+    filenames_list_file_path: str = os.environ['FILENAMES_LIST_FILE_PATH']
+    output_directory_path: str = os.environ['OUTPUT_DIRECTORY_PATH']
+    export_data(hatch_file_path, filenames_list_file_path, output_directory_path)
 
 
 if __name__ == "__main__":
